3c3ae1ec19baa0886e4787e6a8b284535d2e7eb0
Victor Maslov <nakilon@gmail.com>
2022-08-17 07:01:12 +0300

    a bit more full test; fixing github action

--- test.rb
+++ test.rb
@@ -13,7 +13,13 @@ describe Nakischema do
       nil, true, false, :symbol, "string",
       c, /\A\z/, [nil, 1..1],
       {size: 0..0}, {hash_opt: {""=>""}, assertions: [->_,__{ _.empty? }]},
-      {keys: [[4..4, 2..2]], values: [[3..3, 1..1]], keys_sorted: [[2..2, 4..4]], hash_req: {2=>1..1}, each: [[2..4, 1..3]]},
+      {
+        keys: [[4..4, 2..2]], keys_sorted: [[2..2, 4..4]], hash_req: {2=>1..1},
+        each_key: Integer, each_value: Integer,
+        values: [[3..3, 1..1]],
+        each: [[2..4, 1..3]],
+        hash: {4=>3..3, 2=>1..1},
+      },
     ]]
   end
 end
